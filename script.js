833040ef36db21897e04edede1f121c8fbac7912
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-20 17:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function fibonacci(num) {
-if (num == 0) {
-        return 0;
-    } else if (num == 1) {
-        return 1;
-    } else {
-        return fibonacci(num-1) + fibonacci(num-2);
+    if (num <= 1) {
+        return num - 1;
     }
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= num; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return a;
 }
 
 module.exports = fibonacci;
